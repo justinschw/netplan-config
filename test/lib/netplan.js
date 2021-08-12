29b8e62222042100255f74c93dff2b89af8536cf
Justin Schwartzbeck <justinmschw@gmail.com>
2021-08-12 00:19:59 -0500

    Adds framework for code and tests

---
+++ test/lib/netplan.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const netplan = require('../../index');
+
+describe('/lib/netplan', function() {
+    
+    describe('constructor', function() {
+	it('valid', function(done) {
+	    // TODO: test constructor
+	    done();
+	});
+    });
+
+    describe('configureInterface', function() {
+	it('valid', function(done) {
+	    // TODO: test configureInterface
+	    done();
+	});
+    });
+    
+});
