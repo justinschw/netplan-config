675baaa317b8b9bb20641ad7af1bc356a2edd253
Justin Schwartzbeck <justinmschw@gmail.com>
2021-08-12 23:11:06 -0500

    Adds test for netplan apply

--- test/lib/netplan.js
+++ test/lib/netplan.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const mockSpawn = require('mock-spawn');
+const mySpawn = mockSpawn();
+require('child_process').spawn = mySpawn;
 const NetPlan = require('../../index');
 const expect = require('chai').expect;
 const assert = require('chai').assert;
@@ -107,5 +110,56 @@ describe('/lib/netplan', function() {
 			done();
 		});
     });
+
+    describe('apply', function() {
+    	it('success', function(done) {
+    		let code = 0;
+    		let stdout = '';
+    		let stderr = '';
+    		mySpawn.setDefault(mySpawn.simple(code, stdout, stderr));
+
+    		const netplan = new NetPlan();
+    		netplan.binary = '/usr/sbin/netplan';
+
+    		netplan.apply().then(result => {
+    			expect(result.code).eql(code);
+    			done();
+			}).catch(err => {
+				done(err);
+			});
+		});
+
+    	it('failure', function(done) {
+    		let code = -1;
+    		let stdout = '';
+    		let stderr = 'some horrific stack trace';
+    		mySpawn.setDefault(mySpawn.simple(code, stdout, stderr));
+
+    		const netplan = new NetPlan();
+    		netplan.binary = '/usr/sbin/netplan';
+
+    		netplan.apply().then(result => {
+    			done(new Error(`Test failed; expected error but got ${JSON.stringify(result)}`));
+			}).catch(err => {
+				expect(err.code).eql(code);
+				done();
+			});
+		});
+
+		it('no binary', function(done) {
+			let code = -1;
+			let stdout = '';
+			let stderr = 'some horrific stack trace';
+			mySpawn.setDefault(mySpawn.simple(code, stdout, stderr));
+
+			const netplan = new NetPlan();
+
+			netplan.apply().then(result => {
+				done(new Error(`Test failed; expected error but got ${JSON.stringify(result)}`));
+			}).catch(err => {
+				done();
+			});
+		});
+	});
     
 });
